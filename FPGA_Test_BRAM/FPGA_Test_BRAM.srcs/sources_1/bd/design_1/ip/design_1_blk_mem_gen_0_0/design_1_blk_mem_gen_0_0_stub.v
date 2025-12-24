8cdeb2bfc70f0afc8a05141a6e2a094ed15bbf15
Leo <jieliangleo_lin@berkeley.edu>
2025-12-24 12:42:53 -0800

    feat: added the new BRAM design

---
+++ FPGA_Test_BRAM/FPGA_Test_BRAM.srcs/sources_1/bd/design_1/ip/design_1_blk_mem_gen_0_0/design_1_blk_mem_gen_0_0_stub.v
@@ -0,0 +1,27 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+// Date        : Wed Dec 24 11:44:40 2025
+// Host        : Leo running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/linji/FPGA_Test_BRAM/FPGA_Test_BRAM.srcs/sources_1/bd/design_1/ip/design_1_blk_mem_gen_0_0/design_1_blk_mem_gen_0_0_stub.v
+// Design      : design_1_blk_mem_gen_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_4,Vivado 2020.1" *)
+module design_1_blk_mem_gen_0_0(clka, rsta, ena, wea, addra, dina, douta, rsta_busy)
+/* synthesis syn_black_box black_box_pad_pin="clka,rsta,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0],rsta_busy" */;
+  input clka;
+  input rsta;
+  input ena;
+  input [3:0]wea;
+  input [31:0]addra;
+  input [31:0]dina;
+  output [31:0]douta;
+  output rsta_busy;
+endmodule
