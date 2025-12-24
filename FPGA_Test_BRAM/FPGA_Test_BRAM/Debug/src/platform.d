7213c9f6626a47ae466a50f66eb1561606d3368a
Leo <jieliangleo_lin@berkeley.edu>
2025-12-24 12:56:24 -0800

    feat: pushed C code for reading BRAM

---
+++ FPGA_Test_BRAM/FPGA_Test_BRAM/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ C:/Users/linji/OneDrive/Desktop/Test/FPGA_project/FPGA_Test_BRAM/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ C:/Users/linji/OneDrive/Desktop/Test/FPGA_project/FPGA_Test_BRAM/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ C:/Users/linji/OneDrive/Desktop/Test/FPGA_project/FPGA_Test_BRAM/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h \
+ C:/Users/linji/OneDrive/Desktop/Test/FPGA_project/FPGA_Test_BRAM/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ ../src/platform_config.h
+
+C:/Users/linji/OneDrive/Desktop/Test/FPGA_project/FPGA_Test_BRAM/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+C:/Users/linji/OneDrive/Desktop/Test/FPGA_project/FPGA_Test_BRAM/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+
+C:/Users/linji/OneDrive/Desktop/Test/FPGA_project/FPGA_Test_BRAM/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h:
+
+C:/Users/linji/OneDrive/Desktop/Test/FPGA_project/FPGA_Test_BRAM/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+
+../src/platform_config.h:
